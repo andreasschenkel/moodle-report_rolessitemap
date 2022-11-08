c10ab1c10d73896fe7e690d1bf86df75cfd75fcd
Andreas Schenkel <Andreas.Schenkel@web.de>
2022-11-08 16:26:23 +0100

    bump version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2022110700;
+$plugin->version  = 2022110800;
 $plugin->requires = 2021050700; // Requires 3.11.
 $plugin->component = 'report_rolessitemap';
-$plugin->release = '1.0.2';
+$plugin->release = '1.0.3';
 $plugin->maturity  = MATURITY_ALPHA;
