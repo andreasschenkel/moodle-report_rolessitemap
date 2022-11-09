6fa66fe9e1be424c3b4fc1559a1c3c5e35f13b80
Andreas Schenkel <Andreas.Schenkel@web.de>
2022-11-09 17:06:07 +0100

    add new langstrings and sort strings

--- lang/en/report_rolessitemap.php
+++ lang/en/report_rolessitemap.php
@@ -22,12 +22,16 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['configisactive'] = 'In order to use this report by users with the correct capability this must be activated.';
+$string['configisactiveforsiteadmin'] = 'If the report is not activated but the siteadmin should be able to use, then this has to be activated';
+$string['isactive'] = 'Activate report';
+$string['isactiveforsiteadmin'] = 'Active for siteadmin';
+$string['maxcounter'] = 'maximum number of users per role';
+$string['maxcounter_desc'] = "This is the maximum number of users that where shown per role in on coursecategory. This might prevent large lists.";
+$string['pageheader'] = 'Sitemap of categorys and the assigned roles';
 $string['pluginname'] = 'Roles Sitemap';
 $string['privacy:metadata'] = 'The report_rolessitmap plugin does not store any personal data.';
-$string['rolessitemap:view'] = 'View report';
-$string['isactive'] = 'Activate report';
-$string['configisactive'] = 'In order to use this report by users with the correct capability this must be activated.';
-$string['isactiveforsiteadmin'] = 'Active for siteadmin';
-$string['configisactiveforsiteadmin'] = 'If the report is not activated but the siteadmin should be able to use, then this has to be activated';
 $string['reportisinactiv_msg'] = 'Report is not activated or missing capability';
-$string['pageheader'] = 'Sitemap of categorys and the assigned roles';
+$string['rolessitemap:view'] = 'View report';
+$string['supportedroles'] = 'Roles to be supported';
+$string['supportedroles_desc'] = 'The report contains only the selected roles.';
